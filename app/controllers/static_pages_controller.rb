0e28eaa760cfdac755c1967151d3853728d972a7
Jake Will <jwill18@punahou.edu>
2016-10-15 00:11:48 +0000

    Got all pages set up, working on polishing and fixing

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,16 @@
 class StaticPagesController < ApplicationController
   def home
   end
+  
+  def college_rankings
+  end
+  
+  def find_colleges
+  end
+  
+  def most_reviews  
+  end
+  
+  def my_colleges
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get '/my_colleges', to: 'static_pages#my_colleges'
+  get '/most_reviews', to: 'static_pages#most_reviews'
+  get '/find_colleges', to: 'static_pages#find_colleges'
+  get '/college_rankings', to: 'static_pages#college_rankings'
   get 'static_pages/home'
-
-  root 'application#hello'
 end
