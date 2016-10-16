44626b00ec190d24e53b1db3cc6a0c11863f2751
Jake Will <jwill18@punahou.edu>
2016-10-16 22:12:51 +0000

    Added Sign-Up (No login yet)

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get '/most_reviews', to: 'static_pages#most_reviews'
   get '/find_colleges', to: 'static_pages#find_colleges'
   get '/college_rankings', to: 'static_pages#college_rankings'
-  get'/login', to: 'static_pages#login'
-  get 'static_pages/home'
+  get '/login', to: 'static_pages#login'
+  get '/signup', to: 'users#new'
+
+  resources :users
 end
