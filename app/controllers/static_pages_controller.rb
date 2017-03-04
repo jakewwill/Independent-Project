91ada9d76658dd9f5aee1073f40a72b519863742
jwill18 <jwill18@punahou.edu>
2017-03-04 19:50:55 +0000

    Tidying up with pagination

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,7 +12,7 @@ class StaticPagesController < ApplicationController
   end
   
   def most_reviews
-    @colleges = College.order(reviews_count: :desc);
+    @colleges = College.order(reviews_count: :desc).paginate(page: params[:page], per_page: 15, :total_entries => 120)
   end
   
   def my_colleges
