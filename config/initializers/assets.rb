81d10577d48f71a131a5fb5549e45f20282a6d2c
jwill18 <jwill18@punahou.edu>
2017-07-03 05:12:53 +0000

    Condensing CSS

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,7 +3,6 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-Rails.application.config.assets.precompile += %w( awesomplete.css )
 Rails.application.config.assets.precompile += %w( awesomplete.js )
 
 # Add additional assets to the asset load path
