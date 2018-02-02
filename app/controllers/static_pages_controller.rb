267e73bc99e90f3eb0514853f5459e95ca20c949
jwill18 <jwill18@punahou.edu>
2018-02-02 18:34:17 +0000

    Sort Reviews Alphabetically by School

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @reviews = Review.order('college_name ASC')
+    @reviews = Review.order('college_name DESC')
     @reviews = @reviews.paginate(:page => params[:page], :per_page => 15)
   end
   
